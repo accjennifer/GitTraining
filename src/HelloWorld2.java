f2dfff2a6dea9457422eb528e7efd19d354c10a3
Jennifer Li <jennifer.li@acc.co.nz>
2021-03-04 14:09:34 +1300

    ll

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,5 +1,7 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("try again, add to Git manually.");
+        System.out.println("try again, add to Git manually.")
+        System.out.println(" update!");
+        System.out.println(" update again!");
     }
 }
